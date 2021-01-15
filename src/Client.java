1585a70d3c9ed43f1c836ea81c47c08b3a608d48
heiye5 <2716634948@qq.com>
2021-01-15 11:03:40 +0800

    添加注释信息

--- src/Client.java
+++ src/Client.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
+            //创建一个流套接字并将其连接到指定主机上的指定端口号
             Socket socket=new Socket("localhost",8888);
-            InputStream is=socket
-                    .getInputStream();
+
+            //读取服务器端数据
+            InputStream is=socket.getInputStream();
             InputStreamReader isr=new InputStreamReader(is);
             BufferedReader in=new BufferedReader(isr);
+
             String serverMessage=in.readLine();
             System.out.println("服务器端说："+serverMessage);
             Scanner sc = new Scanner(System.in);
             while(true) {
-
+                //向服务器端发送数据
                 System.out.println("请输入你要发送的消息：");
                 String message = sc.next();
                 OutputStream os = socket.getOutputStream();
--- src/SocketDemo.java
+++ src/SocketDemo.java
@@ -5,11 +5,12 @@ import java.net.Socket;
 public class SocketDemo {
     public static void main(String[] args) {
         try {
+            //监听的端口号
             ServerSocket ss=new ServerSocket(8888);
             Socket socket=ss.accept();
             System.out.println("新用户进入系统");
 
-
+            // 向客户端回复信息
             OutputStream os=socket.getOutputStream();
             OutputStreamWriter osw=new OutputStreamWriter(os);
             PrintWriter out=new PrintWriter(osw);
@@ -17,10 +18,13 @@ public class SocketDemo {
             out.flush();
 
             while(true) {
+                // 读取客户端数据
                 InputStream is = socket.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader in = new BufferedReader(isr);
                 String serverMessage = in.readLine();
+
+                // 处理客户端数据
                 System.out.println("客户端说：" + serverMessage);
             }
 
